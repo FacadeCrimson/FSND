ecd27423932157a11aaa649142f13ad884e04782
Lechen Tan <tan.lechen@gmail.com>
2020-06-14 21:13:14 -0400

    feat: stock price with random walk model

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,26 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
+from models import setup_db, Stock, db
+
+# os.system('python try.py')
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+  setup_db(app)
+  migrate = Migrate(app, db)
 
   return app
 
-APP = create_app()
+app = create_app()
+
+
+@app.route('/')
+def get_price():
+  return "Hello World!"
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,59 @@
 
+import os
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "stock_price"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Stock Price
+
+'''
+class Stock(db.Model):  
+  __tablename__ = 'stock'
+
+  id = Column(Integer, primary_key=True)
+  price = Column(Integer,nullable=False)
+  timestamp = Column(DateTime,nullable=False)
+
+#   def __init__(self, question, answer, category, difficulty):
+#     self.question = question
+#     self.answer = answer
+#     self.category = category
+#     self.difficulty = difficulty
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+#   def format(self):
+#     return {
+#       'id': self.id,
+#       'question': self.question,
+#       'answer': self.answer,
+#       'category': self.category,
+#       'difficulty': self.difficulty
+#     }
